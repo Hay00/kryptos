adff032152ad8487e9546c5859e6ff1bbfb4be05
Vinicios Dutra Schulze <vine.vinedutra@hotmail.com>
2021-01-27 03:39:22 -0300

    Add Morse Code

--- src/utils/morseCode.js
+++ src/utils/morseCode.js
@@ -1,9 +1,93 @@
 export default class MorseCode {
+  alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789.,?\'!/()&:;=+-_"$@';
+  morseCode = [
+    '.-',
+    '-...',
+    '-.-.',
+    '-..',
+    '.',
+    '..-.',
+    '--.',
+    '....',
+    '..',
+    '.---',
+    '-.-',
+    '.-..',
+    '--',
+    '-.',
+    '---',
+    '.--.',
+    '--.-',
+    '.-.',
+    '...',
+    '-',
+    '..-',
+    '...-',
+    '.--',
+    '-..-',
+    '-.--',
+    '--..',
+    '-----',
+    '.----',
+    '..---',
+    '...--',
+    '....-',
+    '.....',
+    '-....',
+    '--...',
+    '---..',
+    '----.',
+    '.-.-.-',
+    '--..--',
+    '..--..',
+    '.----.',
+    '-.-.--',
+    '-..-.',
+    '-.--.',
+    '-.--.-',
+    '.-...',
+    '---...',
+    '-.-.-.',
+    '-...-',
+    '.-.-.',
+    '-....-',
+    '..--.-',
+    '.-..-.',
+    '..._.._',
+    '.--.-.',
+  ];
+
   encode(input) {
-    return input;
+    return Array.from(input.toLowerCase().replace(/\n/g, '').split(' '))
+      .map((word) =>
+        Array.from(word)
+          .map((char, index) => {
+            const morseChar = this.morseCode[this.alphabet.indexOf(char)];
+            if (morseChar) {
+              return morseChar;
+            }
+            throw new Error(
+              `Char ${char} at index [${index}] doesn't exist in Morse Code`
+            );
+          })
+          .join(' ')
+      )
+      .join(' / ');
   }
 
   decode(input) {
-    return input;
+    return Array.from(input.split(' / '))
+      .map((word) =>
+        Array.from(word.split(' '))
+          .map((char, index) => {
+            const aplhabetChar = this.alphabet[this.morseCode.indexOf(char)];
+            if (aplhabetChar) {
+              return aplhabetChar;
+            }
+            throw new Error(`Char at index [${index}] caused a parsing error!`);
+          })
+          .join('')
+      )
+      .join(' ');
   }
 }
